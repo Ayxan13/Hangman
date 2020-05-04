bbde3e32fad941e4894c4b0f3c01a23eeaba549a
Ayxan <45293675+Ayxan13@users.noreply.github.com>
2020-05-04 10:41:03 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,5 @@
 #include "hangman.h"
 
 int main() {
-        // frequency sorted English words https://en.wiktionary.org/wiki/Wiktionary:Frequency_lists#English
         Hangman::play("./Wordlist.txt"); 
 }
